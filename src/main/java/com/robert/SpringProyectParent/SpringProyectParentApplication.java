e9ac7fd20885910aff3c24f067aa31a6b664d761
rpaternina <robertpaternina65@gmail.com>
2024-11-15 18:16:29 -0500

    First commit

---
+++ src/main/java/com/robert/SpringProyectParent/SpringProyectParentApplication.java
@@ -0,0 +1,13 @@
+package com.robert.SpringProyectParent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProyectParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProyectParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robert/SpringProyectParent/SpringProyectParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robert.SpringProyectParent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProyectParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
