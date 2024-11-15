f2f6d6f9c5b88fefd35277add19cbab7d688db13
rpaternina <robertpaternina65@gmail.com>
2024-11-15 18:40:26 -0500

    Configuracion y realizacion del microservicio api-gateway

---
+++ microservicio-api-gateway/src/main/java/com/microservicio_gateway/MicroservicioApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.microservicio_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicioApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicioApiGatewayApplication.class, args);
+	}
+
+}
---
+++ microservicio-api-gateway/src/test/java/com/microservicio_gateway/MicroservicioApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicio_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicioApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
